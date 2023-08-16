3dff6f0a83fc563d78cd441dcd2f1e6b5558795f
CriOliveira <141835051+CriOliveira@users.noreply.github.com>
2023-08-16 10:13:26 -0300

    Update projeto_perfil_solo_principal.py
    Nova versão do arquivo principal.

--- src/projeto_perfil_solo_principal.py
+++ src/projeto_perfil_solo_principal.py
@@ -1,20 +1,16 @@
 import sys
-import typing
-
-from PyQt6 import QtCore
 
 from projeto_perfil_solo import Ui_MainWindow
-from PyQt6.QtWidgets import QMainWindow, QApplication, QWidget
+from PyQt6.QtWidgets import QMainWindow, QApplication
 
 class projeto_perfil_solo_principal(Ui_MainWindow, QMainWindow):
     
-    def __init__(self, parent = None) -> Nome:
+    def __init__(self, parent = None) -> None:
         super().__init__(parent)
         super().setupUi(self)
 
 if __name__ == '__main__':
     qt = QApplication(sys.argv)
     projeto_perfil_solo_principal = projeto_perfil_solo_principal()
-    projeto_perfil_solo_principal.show
+    projeto_perfil_solo_principal.show()
     qt.exec()
-       
