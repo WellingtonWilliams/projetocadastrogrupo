c815790d9894c9a93bfff9c9cadb701ba2b6befd
WellingtonWilliams <wwellingtonwilliams@hotmail.com>
2023-08-22 11:48:28 -0300

    Update projeto_perfil_solo_principal.py
    Atual 22 08 11:44

--- src/projeto_perfil_solo_principal.py
+++ src/projeto_perfil_solo_principal.py
@@ -1,6 +1,6 @@
 import sys
 
-from projeto_perfil_solo import Ui_MainWindow
+from projeto_perfil_solo_base import Ui_MainWindow
 from PyQt6.QtWidgets import QMainWindow, QApplication
 
 class Projeto_perfil_solo_principal(Ui_MainWindow, QMainWindow):
@@ -11,6 +11,11 @@ class Projeto_perfil_solo_principal(Ui_MainWindow, QMainWindow):
         self.frame_msg_erro.hide()
         self.stacked_widget.setCurrentWidget(self.page_login)
         self.pushButton_entrar.clicked.connect(self.realizar_login)
+        self.pushButton_fechar_msg_erro.clicked.connect(lambda : self.frame_msg_erro.hide())
+        self.push_button_sair.clicked.connect(self.sair_sistema)
+
+    def sair_sistema(self):
+        self.stacked_widget.setCurrentWidget(self.page_login)
 
     def realizar_login (self):
         login = self.lineEdit_inserir_login.text()
@@ -18,11 +23,15 @@ class Projeto_perfil_solo_principal(Ui_MainWindow, QMainWindow):
         if login == 'cllw' and senha == '128167':
             self.lineEdit_inserir_login.setText('')
             self.lineEdit_inserir_senha.setText('')
-            self.frame_msg_erro.hide('')
+            self.frame_msg_erro.hide()
             self.stacked_widget.setCurrentWidget(self.page_principal)
+            print('Login realizado com sucesso')
+            
         else:
-            self.label_msg_erro.setText('Seu login ou senha estão incorrtos!!!')
-            self.frame_msg_erro.show()  
+            self.label_msg_erro.setText('Seu login ou senha estão incorretos!!!')
+            self.frame_msg_erro.show()
+        
+      
 
 
 if __name__ == '__main__':
@@ -30,4 +39,3 @@ if __name__ == '__main__':
     projeto_perfil_solo_principal = Projeto_perfil_solo_principal()
     projeto_perfil_solo_principal.show()
     qt.exec()
-
